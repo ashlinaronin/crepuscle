96c4f997c20d83c732e362ab19051a63440a8600
ashlinaronin <ashlinaronin@gmail.com>
2019-08-30 09:59:51 -0500

    Add images and fonts

--- src/App.vue
+++ src/App.vue
@@ -16,12 +16,13 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Open+Sans|Zilla+Slab|Zilla+Slab+Highlight&display=swap");
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Open Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,44 @@
 <template>
   <div class="hello">
     <h1>crepuscle</h1>
+    <p>do laser frogs dream of electric sheep?</p>
+    <ul>
+      <li>
+        <img src="../assets/boots-at-night.jpg" alt="galoshes" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "HelloWorld"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+h1 {
+  font-family: "Zilla Slab Highlight", serif;
+  text-transform: uppercase;
+  font-size: 48px;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+img {
+  width: 100%;
 }
 </style>
