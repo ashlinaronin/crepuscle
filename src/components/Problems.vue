1ff234845c06da332c74ab263a873925e9201fae
ashlinaronin <ashlinaronin@gmail.com>
2019-08-30 22:26:19 -0500

    Edit problems copy

--- src/components/Problems.vue
+++ src/components/Problems.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h3>problems and solutions</h3>
+    <h3>problems / solutions</h3>
     <ul>
       <li>
         Infrared camera doesn’t work without infrared illumination
@@ -20,24 +20,26 @@
 </ul>
       </li>
       <li>
-        difficulty connecting to the pi after taking it out of the water (and in general)
-      </li>
-      <li>
-        disk full?
-        running out of space on small sd card (908M of 8gigs left after OS)
-      </li>
-      <li>
-        it gets really hot
-      </li>
-      <li>first field test (on chagres) didn't work
+        Difficulty connecting to the pi after taking it out of the water
         <ul>
-          <li>
-            hypothesis - while battery is fully charged everything works
-          </li>
+          <li>It appears that when I had connected the infrared light to the 5V out on the Pi board, it was drawing a high amount of current (1.8 amps)</li>
+          <li>The Pi itself draws around 80 mA, and the battery provides around 2.1 A at peak capacity</li>
+          <li>Solution was to use a separate battery pack for the light. This </li>
         </ul>
       </li>
       <li>
-        can’t fit two power packs in the same case
+        Running out of space on small sd card (908M of 8gigs left after OS)
+        <ul>
+          <li>Set compression setting to lower so each file takes up less space</li>
+        </ul>
+      </li>
+      <li>
+        It gets really hot
+        <ul>
+          <li>
+            Should be fine in the water :)
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
